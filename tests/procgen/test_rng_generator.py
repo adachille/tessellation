cd60da0ebb99113d6f5390de25136d4f72350973
Anthony D'Achille <adachille@hubspot.com>
2024-09-02 13:14:54 -0400

    Add rng generation tests

---
+++ tests/procgen/test_rng_generator.py
@@ -0,0 +1,42 @@
+import pytest
+from tessellation.procgen.rng.rng_generator import RNGGenerator
+from tessellation.procgen.action import Action
+from tessellation.procgen.generation_result import GenerationResult
+from tessellation.procgen.tessellation_type import TessellationType
+
+
+@pytest.fixture
+def rng_generator():
+    return RNGGenerator(seed=42)
+
+
+def test_generate(rng_generator):
+    side_len = 5
+    generation_result = rng_generator.generate(side_len)
+
+    assert isinstance(generation_result, GenerationResult)
+    assert generation_result.mask.shape == (side_len, side_len)
+    assert generation_result.tessellation_type == TessellationType.SQUARE_TRANSLATION
+
+
+def test_generate_side(rng_generator):
+    side_len = 5
+    action_probs = [0.2, 0.2, 0.2, 0.2, 0.2]
+    mask, action_list = rng_generator._generate_side(side_len, action_probs)
+
+    assert mask.shape == (side_len, side_len)
+    assert all(isinstance(action, Action) for action in action_list)
+
+
+def test_get_rand_action(rng_generator):
+    actions_list = [
+        Action.UP,
+        Action.UP_RIGHT,
+        Action.RIGHT,
+        Action.DOWN,
+        Action.DOWN_RIGHT,
+    ]
+    action_probs = [0.2, 0.2, 0.2, 0.2, 0.2]
+    action = rng_generator._get_rand_action(actions_list, action_probs)
+
+    assert action in actions_list

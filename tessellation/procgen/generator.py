8e9b873ace0fdb49ea5fccf2f6bea4b42f44b981
Anthony D'Achille <adachille@hubspot.com>
2024-09-02 13:12:56 -0400

    Fix bug in generator.py `_draw_line` function

--- tessellation/procgen/generator.py
+++ tessellation/procgen/generator.py
@@ -25,16 +25,16 @@ class Generator(ABC):
         cursor = {"x": start_point[1], "y": start_point[0]}
         mask[cursor["y"], cursor["x"]] = 1
         for action in action_list:
-            if cursor["y"] >= 0:
-                mask[0 : cursor["y"], cursor["x"]] = 1
-            else:
-                mask[cursor["y"] :, cursor["x"]] = 1
-
             if action in [Action.UP_RIGHT, Action.UP]:
                 cursor["y"] -= 1
             if action in [Action.DOWN_RIGHT, Action.DOWN]:
                 cursor["y"] += 1
             if action in [Action.UP_RIGHT, Action.DOWN_RIGHT, Action.RIGHT]:
                 cursor["x"] += 1
+
+            if cursor["y"] >= 0:
+                mask[0 : cursor["y"] + 1, cursor["x"]] = 1
+            else:
+                mask[cursor["y"] :, cursor["x"]] = 1
 
         return mask

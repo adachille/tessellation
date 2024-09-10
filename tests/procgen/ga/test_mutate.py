255aeea09274ed01924c3f15226b08f1116dd6d9
Anthony D'Achille <adachille@hubspot.com>
2024-09-10 15:37:21 -0400

    Add test for mutate functions

---
+++ tests/procgen/ga/test_mutate.py
@@ -0,0 +1,86 @@
+import numpy as np
+import pytest
+from tessellation.procgen.ga.mutate import (
+    apply_mutation,
+    substitute_action,
+    insert_action,
+    delete_action,
+)
+from tessellation.procgen import Action, ALL_ACTIONS
+from tessellation.procgen.ga.genome import TessellationGenome
+from leap_ec import Individual
+
+
+@pytest.fixture
+def mock_actions():
+    return [Action.UP, Action.RIGHT]
+
+
+@pytest.fixture
+def mock_individual(mock_actions):
+    genome = TessellationGenome(mock_actions, (0, 0))
+    return Individual(genome)
+
+
+def test_apply_mutation(mock_individual):
+    def mock_mutation_fn(action: Action, **kwargs) -> list[Action]:
+        return [Action.DOWN]
+
+    individual_iterator = iter([mock_individual])
+    mutated_individual = next(apply_mutation(mock_mutation_fn, individual_iterator))
+
+    assert mutated_individual.genome.actions == [
+        Action.DOWN,
+        Action.DOWN,
+    ]
+    assert mutated_individual.fitness is None
+
+
+def test_substitute_action(mock_actions):
+    action = mock_actions[0]
+    new_actions = substitute_action(action, substitution_prob=1.0)
+    assert all(a in ALL_ACTIONS for a in new_actions)
+
+    new_actions = substitute_action(action, substitution_prob=0.0)
+    assert new_actions == [action]
+
+
+def test_substitution_action_uses_action_probs(mock_actions):
+    action_probs = np.zeros(len(ALL_ACTIONS)).tolist()
+    action_probs[-1] = 1
+    new_actions = substitute_action(
+        mock_actions[0], substitution_prob=1.0, action_probs=action_probs
+    )
+    assert new_actions == [ALL_ACTIONS[-1]]
+
+
+def test_insert_action(mock_actions):
+    action = mock_actions[0]
+    new_actions = insert_action(action, insertion_prob=1.0)
+    assert all(a in ALL_ACTIONS for a in new_actions)
+    assert len(new_actions) == 2
+
+    new_actions = insert_action(action, insertion_prob=0.0)
+    assert new_actions == [action]
+    assert len(new_actions) == 1
+
+
+def test_insert_action_uses_action_probs(mock_actions):
+    action_probs = np.zeros(len(ALL_ACTIONS)).tolist()
+    action_probs[-1] = 1
+    new_actions = insert_action(
+        mock_actions[0], insertion_prob=1.0, action_probs=action_probs
+    )
+    assert new_actions in [
+        [mock_actions[0], ALL_ACTIONS[-1]],
+        [ALL_ACTIONS[-1], mock_actions[0]],
+    ]
+
+
+def test_delete_action(mock_actions):
+    action = mock_actions[0]
+    new_actions = delete_action(action, deletion_prob=1.0)
+    assert new_actions == []
+
+    new_actions = delete_action(action, deletion_prob=0.0)
+    assert new_actions == [action]

a47e57f3463564c4b7dc8d33ece3eab14aaf5f5e
adachille <adachille15@gmail.com>
2024-09-29 16:00:38 -0400

    Fix tests

--- tests/procgen/ga/test_problem.py
+++ tests/procgen/ga/test_problem.py
@@ -7,22 +7,24 @@ from tessellation.procgen.generator import Point
 
 @pytest.fixture
 def genome():
-    actions = [Action.UP_RIGHT, Action.DOWN_RIGHT, Action.DOWN_RIGHT]
+    actions = [Action.UP_RIGHT, Action.DOWN, Action.DOWN_RIGHT]
     start_point = Point(0, 0)
-    yield TessellationGenome(actions, start_point)
+    yield TessellationGenome(actions, start_point, end_point=Point(3, 0))
 
 
 @pytest.fixture
 def phenome():
-    line_indices = [Point(x=x, y=y) for x, y in [(0, 0), (1, -1), (2, 0), (4, 1)]]
+    line_indices = [
+        Point(x=x, y=y) for x, y in [(0, 0), (1, -1), (2, 0), (3, 1), (3, 0)]
+    ]
     yield TessellationPhenome(line_indices=line_indices)
 
 
 @pytest.fixture
 def population():
     yield [
-        TessellationGenome([Action.UP, Action.RIGHT], Point(0, 0)),
-        TessellationGenome([Action.DOWN, Action.LEFT], Point(1, 1)),
+        TessellationGenome([Action.UP, Action.RIGHT], Point(0, 0), Point(5, 0)),
+        TessellationGenome([Action.DOWN, Action.RIGHT], Point(1, 1), Point(5, 0)),
     ]
 
 
@@ -53,5 +55,6 @@ def test_worse_than(problem):
 def test_initialize_genome(problem):
     initialized_genome = initialize_genome(problem)
     assert initialized_genome.start_point == Point(0, 0)
+    assert initialized_genome.end_point == Point(3, 0)
     assert all(action in Action for action in initialized_genome.actions)
-    assert len(initialized_genome.actions) == problem.side_len
+    assert len(initialized_genome.actions) == problem.side_len - 1
